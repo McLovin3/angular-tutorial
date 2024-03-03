a198a3358255033bf1b6cf9be5aedb151611be51
mclovin <mathieu.ford@gmail.com>
2024-03-03 17:06:03 -0500

    Implemented housing details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Housinglocation } from "../housinglocation";
+import { HousingService } from "../housing.service";
 
 @Component({
   selector: "app-details",
@@ -7,4 +10,16 @@ import { Component } from "@angular/core";
   templateUrl: "./details.component.html",
   styleUrl: "./details.component.css",
 })
-export class DetailsComponent {}
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocationId: number = -1;
+  housingLocation?: Housinglocation;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation = this.housingService.getHousingLocationById(
+      this.housingLocationId
+    );
+  }
+}
