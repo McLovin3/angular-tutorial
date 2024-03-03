3a9b50aa8c4e0f0f603bbb1de9dcf40ef8adc59b
mclovin <mathieu.ford@gmail.com>
2024-03-03 16:53:11 -0500

    Formatting changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+import { CommonModule } from "@angular/common";
+import { HousingService } from "../housing.service";
+import { Housinglocation } from "../housinglocation";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  imports: [HousingLocationComponent, CommonModule],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
